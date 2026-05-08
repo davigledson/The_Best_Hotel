a7e81569ebfdd6677e2a6c5f57e059a0ec27c9c9
GabO-y <gabrielosilva1000@gmail.com>
2026-05-08 09:49:28 -0300

    backend initialization

---
+++ backend/src/main/java/com/the/best/hotel/theBestHotel/TheBestHotelApplication.java
@@ -0,0 +1,13 @@
+package com.the.best.hotel.theBestHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestHotelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/the/best/hotel/theBestHotel/TheBestHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the.best.hotel.theBestHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBestHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
